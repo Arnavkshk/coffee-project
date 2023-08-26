424cb36603ec7cd94a63dfcfb8450aa8ea54b8b0
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-08-27 01:21:55 +0530

    added bootstrap

--- front-end/src/components/navbar.js
+++ front-end/src/components/navbar.js
@@ -1,11 +1,48 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
-const navbar = () => {
+
+const Navbar = () => {
   return (
-    <div>
-      
+    <>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <Link class="navbar-brand" href="#">COFFEE TIME</Link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <Link className="nav-link active" aria-current="page" href="#">Home</Link>
+          </li>
+          <li class="nav-item">
+            <Link className="nav-link" href="#">Link</Link>
+          </li>
+          <li class="nav-item dropdown">
+            <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </Link>
+            <ul class="dropdown-menu">
+              <li><Link className="dropdown-item" href="#">Action</Link></li>
+              <li><Link className="dropdown-item" href="#">Another action</Link></li>
+              <li><hr class="dropdown-divider"/></li>
+              <li><Link className="dropdown-item" href="#">Something else here</Link></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <Link className="nav-link disabled" aria-disabled="true">Disabled</Link>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
     </div>
+  </nav>
+    </>
   )
 }
 
-export default navbar
+export default Navbar;
