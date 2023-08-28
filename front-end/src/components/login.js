bfbe3b9d5bcd1a627a9576474b8baa7aee3651c1
Ansh Ojha <anshojha24@gmail.com>
2023-08-28 16:10:57 +0530

    Login page component added and some styling is left

--- front-end/src/components/login.js
+++ front-end/src/components/login.js
@@ -3,30 +3,50 @@ import image1 from "../assets/WhatsApp Image 2023-08-27 at 00.40.27.jpg";
 function login() {
   return (
     <div className="relative">
-      {/* <div className="">
+      <div className="">
       <img
             className="h-screen w-screen"
             src={image1}
             alt=""
           />
-      </div> */}
+      </div>
 
-     
-        <div className=" border border-black h w-fit grid place-items-center top-0">
-          <div>
-            <h1 className="drop-shadow-2xl w-1/3 h-2/3 ">COFEE TIME</h1>
-          </div>
+      <div className=" absolute border border-white bg-white h-max w-max	 grid place-items-center top-20 right-20">
+        <divc className="my-0">
+          <h1 className="font-bold text-4xl m-2 grid place-items-center my-4 text-amber-700">
+            COFEE TIME
+          </h1>
+        </divc>
 
-          <div className="credentials ">
-            <input type="email" name="" id="" placeholder="Enter your Email" />
-            <input type="password" name="" id="" placeholder="Password" />
-          </div>
-
-          <div>
-            <button type="submit">LogIN</button>
-          </div>
+        <div className="block flex flex-col space-y-6 m-10 w-">
+          <input
+            className="border border-black p-1 w-max rounded-[7px] px-3 py-2 font-sans text-base"
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            className=" border border-black p-1 rounded-[7px] px-3 py-2 font-sans text-base"
+            type="password"
+            placeholder="Password"
+          />
         </div>
-     
+        <div className="flex ml-10  space-x-2 text-black items-center">
+              <input
+                className="rounded-[7px] py-4 w-10"
+                type="checkbox"
+                class="default:ring-2 ..."
+              />
+              <p className="text-black">
+                Agree with
+                <span className="underline px-1 text-black">Terms and Conditions</span>
+              </p>
+            </div>
+            <div className="flex justify-center items-center mt-4">
+              <button className="rounded-[6px] h-10 w-12 bg-orange-600">
+                login
+              </button>
+            </div>
+      </div>
     </div>
   );
 }
