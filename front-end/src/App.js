a693a2322dfa345f0fc2abb13f7ca91371c6f911
Ansh Ojha <anshojha24@gmail.com>
2023-08-31 23:47:16 +0530

    baku

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
-// import Navbar from './components/Navbar';
-import Login from './components/Login';
+import Navbar from './components/Navbar';
+import Login from './components/Login'
 
 function App() {
   return (
     <>
     <div className="App">
     <Login/>
-    {/* <Navbar/> */}
+    <Navbar/>
     </div>
     </>
   );
